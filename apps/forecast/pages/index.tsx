46e68a806da1e3c0e1e296750ebe206cc3f6c325
Mark Ledwich <mark@ledwich.com.au>
2023-10-13 09:48:57 +1000

    isolated modules is weird

--- apps/forecast/pages/index.tsx
+++ apps/forecast/pages/index.tsx
@@ -1,16 +1,16 @@
-import { Button, Group, MantineContext } from "@mantine/core";
+import { Button, Flex, Group, MantineContext } from "@mantine/core";
 import { useContext } from "react";
 import { ThemeTest } from "shared";
 export default function IndexPage() {
-  const ctx = useContext(MantineContext)
+  const ctx = useContext(MantineContext);
   return (
     <Group justify="center">
-      <Group mt={50} bg="blue">
-        App Mantine Context: {ctx ? 'Exists' : 'Missing'}
-      </Group>
-      <Group mt={50} bg="red">
-        <ThemeTest />
-      </Group>
+      <Flex direction={"column"}>
+        <Group mt={50}>App Mantine Context: {ctx ? "Exists" : "Missing"}</Group>
+        <Group mt={50}>
+          <ThemeTest />
+        </Group>
+      </Flex>
     </Group>
   );
 }
